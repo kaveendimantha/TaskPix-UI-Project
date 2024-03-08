83d9da703da3233392d47860868a52c26c3d9d49
Kaveen Dimantha <kaveendimantha@gmail.com>
2024-03-08 12:51:48 +0530

    login and signup create

--- app/src/main/java/com/example/taskpix/Modules/signup.kt
+++ app/src/main/java/com/example/taskpix/Modules/signup.kt
@@ -3,20 +3,18 @@ package com.example.taskpix.Modules
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.TextView
 import com.example.taskpix.R
 
-class login : AppCompatActivity() {
+class signup : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_signup)
 
-        val btnNavigate: TextView = findViewById(R.id.btnsignin)
+        val btnNavigate: TextView = findViewById(R.id.btnlog)
         btnNavigate.setOnClickListener {
-            val intent = Intent(this, signup::class.java)
+            val intent = Intent(this, login::class.java)
             startActivity(intent)
         }
-
     }
 }
